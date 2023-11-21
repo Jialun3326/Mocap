84de935be0a4c8f6f8808248a27794194488b2d2
Jialun Li (jl11u20) <jl11u20@soton.ac.uk>
2023-11-21 16:45:46 +0000

    Implemented the scene 1 switch

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scene 2");
+    }
+    public void GoToSettingMenu()
+    {
+        SceneManager.LoadScene("SettingMenu");
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
